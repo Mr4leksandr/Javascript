f4ab2045c6992b6e0294f93e5363ff2c1e8a3b44
Opilane TTHK <opilane@tthk.ee>
2019-12-09 10:52:09 +0200

    co

--- app.js
+++ app.js
@@ -46,3 +46,49 @@ mixedArray.forEach(element => {
 });
 
 console.log("Banana is a ",mixedArray[1].length,"chars long");
+
+mixedArray.push("apple");
+
+mixedArray.unshift(1000);
+
+mixedArray.pop();
+
+mixedArray.shift();
+
+mixedArray.splice(1,1);
+
+mixedArray.forEach(element=>{
+    console.log("Updated array: ",element);
+});
+
+const person = {
+    firstName: "John",
+    lastName: "Doe",
+    age: 25,
+    email: "john.doe@gmail.com",
+    hobbies: ["music","sports"],
+
+    getYearOfBirth: function() {
+        return 2019- this.age;
+    }
+}
+
+let val;
+val = person;
+val = person.firstName;
+val = person.lastName;
+val = person["lastName"];
+val = person.age;
+val = person.hobbies[1];
+val = person.getYearOfBirth();
+console.log(val);
+
+const people = [
+    {name: "John", age: 25},
+    {name: "Sarah", age: 28},
+    {name: "Tom", age: 18},
+]
+
+people.forEach(person=>{
+    console.log(person.name, "is", person.age, " years old");
+});
